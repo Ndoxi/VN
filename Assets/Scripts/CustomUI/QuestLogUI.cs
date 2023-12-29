b3bb174625d484508894ec972a94d470641f51fa
Ndoxi <90957973+Ndoxi@users.noreply.github.com>
2023-12-29 18:15:28 +0200

    Finished main quest line

---
+++ Assets/Scripts/CustomUI/QuestLogUI.cs
@@ -0,0 +1,12 @@
+using Naninovel.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VN.UI
+{
+    public class QuestLogUI : CustomUI
+    {
+
+    }
+}
---
+++ Assets/Scripts/Services/QuestLogService.cs
@@ -0,0 +1,49 @@
+using Naninovel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VN.UI;
+
+namespace VN.Services
+{
+    [InitializeAtRuntime]
+    [Naninovel.Commands.Goto.DontReset]
+    public class QuestLogService : IEngineService
+    {
+        private readonly List<int> _log;
+        private readonly UIManager _uIManager;
+        private QuestLogUI _questLogUI;
+
+        public QuestLogService(UIManager uIManager)
+        {
+            _log = new List<int>();
+            _uIManager = uIManager;
+        }
+
+        public void DestroyService()
+        {
+            _log.Clear();
+        }
+
+        public void ResetService()
+        {
+            _log.Clear();
+        }
+
+        public UniTask InitializeServiceAsync()
+        {
+            _questLogUI = _uIManager.GetUI<QuestLogUI>();
+            return UniTask.CompletedTask;
+        }
+
+        public void Add(int questStage)
+        {
+            _log.Add(questStage);
+        }
+
+        public void Clear()
+        {
+            _log.Clear();
+        }
+    }
+}
